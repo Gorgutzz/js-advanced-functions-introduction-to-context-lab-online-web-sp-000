3956d357107c95f5fcafbb2fd18469d9122f7d1c
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-10-11 00:29:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 // Your code here
-function createEmployeeRecord([firstName, familyName, title, payHourly])
+function createEmployeeRecord([firstName, familyName, title, payHourly]) {
+  return employeeRecord = {
+    firstName = []
+    familyName
+    title
+  }
+}
